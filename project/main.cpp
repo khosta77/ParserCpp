9e1a76ec61bc7452105f3f64496fd396ec11d7ed
khosta77 <puwerfulpants@gmail.com>
2024-10-12 00:31:55 +0300

    Можно считать данные с лабиринта

--- project/main.cpp
+++ project/main.cpp
@@ -5,12 +5,15 @@
 
 #include <curl/curl.h>
 
-
-//const std::string URL = "https://ru.investing.com/indices/us-spx-500";
 const std::string URL = "https://www.labirint.ru/books/877234/";
-//const std::string URL = "https://ru.investing.com/search";
 
 class Request
+{
+public:
+    virtual std::string GET( const std::string& ) = 0;
+};
+
+class RequestCurl : public Request
 {
 private:
     CURL *curl;
@@ -20,28 +23,23 @@ private:
     static size_t WriteCallback( void* contents, size_t size, size_t nmemb, std::string* output )
     {
         size_t total_size = size * nmemb;
-        output->append(static_cast<char*>(contents), total_size);
+        output->append( static_cast<char*>(contents), total_size );
         return total_size;
     }
 
 public:
-    Request() : content("")
+    RequestCurl() : content("")
     {
         curl_global_init(CURL_GLOBAL_DEFAULT);
         curl = curl_easy_init();
     }
 
-    ~Request()
+    ~RequestCurl()
     {
         curl_global_cleanup();
     }
     
-    Request( const Request& rhs ) = delete;
-    Request( Request&& rhs ) = delete;
-    Request& operator=( const Request& rhs ) = delete;
-    Request& operator=( Request&& rhs ) = delete;
-
-    std::string GET( const std::string& url )
+    std::string GET( const std::string& url ) override
     {
         if(curl)
         {
@@ -60,81 +58,16 @@ public:
     }
 };
 
-#if 0
-namespace beast = boost::beast;          // изолируем экспозицию boost::beast
-namespace http = beast::http;            // изолируем экспозицию boost::beast::http
-namespace net = boost::asio;              // изолируем экспозицию boost::asio
-using tcp = net::ip::tcp;                // изолируем экспозицию boost::asio::ip::tcp
-
-// Функция для выполнения GET запроса
-std::string http_get(const std::string& url) {
-    // Разделение URL на компоненты
-    auto const pos = url.find("://");
-    if (pos == std::string::npos) {
-        throw std::runtime_error("Invalid URL: " + url);
+std::vector<std::string> getKeyWords( const std::vector<std::pair<std::string, std::string>>& pairs )
+{
+    std::vector<std::string> keys( pairs.size() );
+    size_t i = 0;
+    for( const std::pair<std::string, std::string>& pair : pairs )
+    {
+        keys[i++] = ( pair.first + "=\"" + pair.second + "\"" );
     }
-
-    // Получаем протокол и сдвигаем указатель после "://"
-    std::string protocol = url.substr(0, pos);
-    std::string remaining_url = url.substr(pos + 3);
-
-    // Разделяем оставшуюся часть на имя хоста и путь
-    auto const path_start = remaining_url.find('/');
-    std::string host = (path_start == std::string::npos) ? remaining_url : remaining_url.substr(0, path_start);
-    std::string path = (path_start == std::string::npos) ? "/" : remaining_url.substr(path_start);
-
-    // Создаем контекст io и сокет
-    net::io_context io_context;
-
-    // Разрешаем имя хоста
-    tcp::resolver resolver(io_context);
-    auto const results = resolver.resolve(host, protocol == "https" ? "443" : "80");
-
-    // Создаем и заполняем HTTP-запрос
-    beast::tcp_stream stream(io_context);
-    net::connect(stream.socket(), results.begin(), results.end());
-
-    // Если используем https, оборачиваем в secure stream
-    if (protocol == "https") {
-        stream = beast::ssl_stream<beast::tcp_stream>(std::move(stream), boost::asio::ssl::context(boost::asio::ssl::context::sslv23));
-        boost::asio::ssl::stream_base::handshake_type handshake_type = boost::asio::ssl::stream_base::client;
-        static_cast<beast::ssl_stream<beast::tcp_stream>&>(stream).handshake(handshake_type);
-    }
-
-    // Составляем запрос
-    http::request<http::string_body> req(http::verb::get, path, 11);
-    req.set(http::field::host, host);
-    req.set(http::field::user_agent, "Boost.Beast/1.81.0"); // Укажите вашу версию Boost.Beast
-
-    // Отправляем запрос
-    http::write(stream, req);
-
-    // Получаем ответ
-    beast::flat_buffer buffer;
-    http::response<http::string_body> res;
-    http::read(stream, buffer, res);
-
-    // Закрываем соединение
-    beast::error_code ec;
-    stream.socket().shutdown(tcp::socket::shutdown_both, ec);
-
-    // Если ошибка, выводим её
-    if (ec && ec != beast::errc::not_connected) {
-        throw beast::system_error{ec};
-    }
-
-    // Возвращаем ответ
-    return res.body();
+    return keys;
 }
-#endif
-
-
-//<a class="js-inner-all-results-quote-item row" href="/equities/mts_rts">
-//  <span class="flag first"><i class="ceFlags middle Russian_Federation"></i></span>
-//  <span class="second">MTSS</span>
-//  <span class="third">Мобильные ТелеСистемы ПАО</span>
-//  <span class="fourth">Aкция - Москва equities</span>
-//</a>
 
 std::pair<std::string, size_t> getBlockTitle( const std::string& content, const size_t& start )
 {
@@ -149,104 +82,74 @@ std::pair<std::string, size_t> getBlockTitle( const std::string& content, const 
     return std::pair<std::string, size_t>( returnMessage, i );
 }
 
-std::string parseHref( const std::string& content, const std::string& _class = "div",
-        const std::string& _code = "id", const std::string _value = "fullannotation" )
+bool isKeysNpos( const std::string& head, const std::vector<std::string>& keys )
 {
-    const std::string triggerLeft = ( "<" + _class );
-    const std::string triggerRight = ( "</" + _class + ">" );
-    const std::string key = ( _code + "=\"" + _value + "\"" );
+    for( const std::string& key : keys )
+    {
+        if( head.find( key ) == std::string::npos )
+            return true;
+    }
+    return false;
+}
+
+std::string parseHref(
+        const std::string& content,
+        const std::string& object,
+        const std::vector<std::pair<std::string, std::string>> pairs
+    )
+{
+    const std::string triggerLeft = ( "<" + object );
+    const std::string triggerRight = ( "</" + object + ">" );
+    const std::vector<std::string> keys = getKeyWords( pairs );  // ( _code + "=\"" + object + "\"" );
+
     std::string message = "";
+#if 1
     size_t count = 0;
-    size_t i = 0, j = 0;
+    size_t i = 0;
+    size_t j = 0;
+
+    size_t posNextDiv = 0;
+    size_t posEndCurrent = 0;
+
     while( ( i = content.find( triggerLeft, i ) ) != std::string::npos )
     {
         auto block = getBlockTitle( content, i );
         i = block.second;
 
-        if( block.first.find( key ) == std::string::npos )
+        if( isKeysNpos( block.first, keys ) )
             continue;
 
-        std::cout << block.first << std::endl;
-    }
-/*
-    for( size_t i = 0; i < content.size(); ++i )
-    {
-        if( content[i] =='<' )
+        //std::cout << block.first << std::endl;
+        j = i;
+
+        for( size_t J  = content.size(); j < J; j = posEndCurrent )
         {
-            auto block = getBlockTitle( content, i );
-            std::cout << block.first << std::endl;
-            i = block.second;
+            posNextDiv = content.find( triggerLeft, j );
+            posEndCurrent = content.find( triggerRight, j );
 
-            if( block.first.find( triggerLeft ) != std::string::npos )
-            {
-                if( message.empty() )
-                    continue;
-                
-                message += block.first;
-                --count;
-                if( count == 0 )
-                    break;
-            }
+            if( posEndCurrent == std::string::npos )
+                throw;  // Ломанный файл
 
-            if( block.first.find( triggerLeft ) == std::string::npos )
-            {
-                if( !message.empty() )
-                    message += block.first;
-                continue;
-            }
-            
-            if( block.first.find( key ) == std::string::npos )
-            {
-                if( !message.empty() )
-                    message += block.first;
-                continue;
-            }
-
-            if( !message.empty() )
-                ++count;
-
-            message += block.first;
+            if( ( posNextDiv == std::string::npos ) or ( posNextDiv > posEndCurrent ) )
+                break;
         }
 
-        if( !message.empty() )
-            message += content[i];
+        message = content.substr( i, ( posEndCurrent - i ) );
+        break;
     }
-*/
-    return message;
-#if 0
-    std::regex regex( R"(<div[^>]*id=["']fullannotation["'][^>]*>(.*?)</div>)" );
-    std::smatch match;
-
-    if( std::regex_search(content, match, regex) && match.size() > 1 )
-	{
-        std::string href = match[0].str();
-        return href;
-    }
-    throw;
 #endif
+    return message;
 }
 
-float parsePrice( const std::string& content )
-{
-    std::regex regex( R"(<div[^>]*data-test=["']instrument-price-last["'][^>]*>(.*?)</div>)" );
-    std::smatch match;
-
-    if( std::regex_search(content, match, regex) && match.size() > 1 )
-	{
-        std::string priceStr = match[1].str();
-        priceStr.erase( std::remove( priceStr.begin(), priceStr.end(), '.' ), priceStr.end() );
-        std::replace( priceStr.begin(), priceStr.end(), ',', '.' );
-        return std::stof(priceStr);
-    }
-    throw;
-}
-
-Request request;
+RequestCurl request;
 
 int main()
 {
     std::string content = request.GET(URL);
-    std::cout << parseHref(content) << std::endl;
+    //std::cout << parseHref( content, "div", { { "id", "fullannotation" } } ) << std::endl;
+    //std::cout << parseHref( content, "span", { { "class", "buying-pricenew-val-number" } } ) << std::endl;
+    std::cout << parseHref( content, "div", { { "id", "product-image" } } ) << std::endl;
+//    std::cout << parseHref( content, "", { { "", "" } } ) << std::endl;
     return 0;
 }
 
