a2fe749d56f20733188af9d7bc2cf6fe2bd0725c
khosta77 <puwerfulpants@gmail.com>
2024-10-13 18:40:32 +0300

    Исправил ошибку с ловлей больших блоков(они не ловились). Подсократил код, вдебагерил вывод(ужасно выглядит, но что есть). Обернул все в класс SCBeautifulSoup

--- project/main.cpp
+++ project/main.cpp
@@ -5,6 +5,8 @@
 
 #include <curl/curl.h>
 
+#define DEBUG 0
+
 const std::string URL = "https://www.labirint.ru/books/877234/";
 
 class Request
@@ -58,98 +60,148 @@ public:
     }
 };
 
-std::vector<std::string> getKeyWords( const std::vector<std::pair<std::string, std::string>>& pairs )
-{
-    std::vector<std::string> keys( pairs.size() );
-    size_t i = 0;
-    for( const std::pair<std::string, std::string>& pair : pairs )
-    {
-        keys[i++] = ( pair.first + "=\"" + pair.second + "\"" );
-    }
-    return keys;
-}
 
-std::pair<std::string, size_t> getBlockTitle( const std::string& content, const size_t& start )
+class SCBeautifulSoup
 {
-    std::string returnMessage = "";
-    size_t i = start;
-    for( ; i < content.size(); )
+private:
+    std::vector<std::string> getKeyWords( const std::vector<std::pair<std::string, std::string>>& pairs )
     {
-        returnMessage += content[i];  // TODO: переделать
-        if( content[i++] == '>' )
-            break;
+        std::vector<std::string> keys( pairs.size() );
+        size_t i = 0;
+        for( const std::pair<std::string, std::string>& pair : pairs )
+        {
+            keys[i++] = ( pair.first + "=\"" + pair.second + "\"" );
+        }
+        return keys;
     }
-    return std::pair<std::string, size_t>( returnMessage, i );
-}
 
-bool isKeysNpos( const std::string& head, const std::vector<std::string>& keys )
-{
-    for( const std::string& key : keys )
+    std::pair<std::string, size_t> getBlockTitle( const std::string& content, const size_t& start )
     {
-        if( head.find( key ) == std::string::npos )
-            return true;
+        std::string returnMessage = "";
+        size_t i = start;
+        for( ; i < content.size(); )
+        {
+            returnMessage += content[i];  // TODO: переделать
+            if( content[i++] == '>' )
+                break;
+        }
+        return std::pair<std::string, size_t>( returnMessage, i );
     }
-    return false;
-}
 
-std::string parseHref(
+    bool isKeysNpos( const std::string& head, const std::vector<std::string>& keys )
+    {
+        for( const std::string& key : keys )
+        {
+            if( head.find( key ) == std::string::npos )
+                return true;
+        }
+        return false;
+    }
+
+    size_t findEndObject( const std::string& content, const size_t& start,
+                          const std::string& tL, const std::string& tR )
+    {
+        size_t i = start;
+
+        size_t count_object_open = 1;
+        size_t count_object_end = 0;
+
+        size_t posNextObject = 0;
+        size_t posEndCurrent = 0;
+
+        for( size_t I = content.size(); i < I; )
+        {
+            posNextObject = content.find( tL, i );
+            posEndCurrent = content.find( tR, i );
+
+            if( posEndCurrent == std::string::npos )
+                return std::string::npos;  // Ломанный файл, вернуть ноль
+
+            if( posNextObject == std::string::npos )
+                return posEndCurrent;
+
+            if( posNextObject > posEndCurrent )
+            {
+#if DEBUG
+                std::cout << posNextObject << '>' << posEndCurrent << std::endl;
+#endif
+                i = ( posEndCurrent + 1 );
+                ++count_object_end;
+            }
+            else
+            {
+#if DEBUG
+                std::cout << posNextObject << '<' << posEndCurrent << std::endl;
+#endif
+                i = ( posNextObject + 1 );
+                ++count_object_open;
+            }
+
+            if( count_object_open == count_object_end )
+                return posEndCurrent;
+        }
+    
+        return std::string::npos;
+    }
+
+public:
+    SCBeautifulSoup() = default;
+    SCBeautifulSoup(int) = delete;
+    SCBeautifulSoup( const SCBeautifulSoup& ) = delete;
+    SCBeautifulSoup( SCBeautifulSoup&& ) = delete;
+    ~SCBeautifulSoup() = default;
+
+    std::string parseHref
+    (
         const std::string& content,
         const std::string& object,
         const std::vector<std::pair<std::string, std::string>> pairs
     )
-{
-    const std::string triggerLeft = ( "<" + object );
-    const std::string triggerRight = ( "</" + object + ">" );
-    const std::vector<std::string> keys = getKeyWords( pairs );  // ( _code + "=\"" + object + "\"" );
-
-    std::string message = "";
-#if 1
-    size_t count = 0;
-    size_t i = 0;
-    size_t j = 0;
-
-    size_t posNextDiv = 0;
-    size_t posEndCurrent = 0;
-
-    while( ( i = content.find( triggerLeft, i ) ) != std::string::npos )
     {
-        auto block = getBlockTitle( content, i );
-        i = block.second;
+        // Триггеры начала и конца блока
+        const std::string tL = ( "<" + object );
+        const std::string tR = ( "</" + object + ">" );
 
-        if( isKeysNpos( block.first, keys ) )
-            continue;
+        // Ключевые слова
+        const std::vector<std::string> keys = getKeyWords( pairs );
+        std::pair<std::string, size_t> block{ "", 0 };
 
-        //std::cout << block.first << std::endl;
-        j = i;
-
-        for( size_t J  = content.size(); j < J; j = posEndCurrent )
+        while( ( block.second = content.find( tL, block.second ) ) != std::string::npos )
         {
-            posNextDiv = content.find( triggerLeft, j );
-            posEndCurrent = content.find( triggerRight, j );
+            block = getBlockTitle( content, block.second );
 
-            if( posEndCurrent == std::string::npos )
-                throw;  // Ломанный файл
-
-            if( ( posNextDiv == std::string::npos ) or ( posNextDiv > posEndCurrent ) )
-                break;
-        }
-
-        message = content.substr( i, ( posEndCurrent - i ) );
-        break;
-    }
+            if( !isKeysNpos( block.first, keys ) )
+            {
+#if DEBUG
+                std::cout << block.first << std::endl;
 #endif
-    return message;
-}
+                size_t posEndCurrent = findEndObject( content, block.second, tL, tR );
+                if( posEndCurrent == std::string::npos )
+                    return std::string("");
+                return content.substr( block.second, ( posEndCurrent - block.second ) );
+            }
+        }
+        return std::string("");
+    }
+};
 
 RequestCurl request;
+SCBeautifulSoup scbs;
+/*
+План парсинга такой, у нас есть большой скрин всей страницы размер N, извлекаем из него маленький блок
+размера M, где M << N и проходимся по нему мелким поиском. В случае когда мы не находим ничего empty = 1.
 
+
+*/
 int main()
 {
     std::string content = request.GET(URL);
     //std::cout << parseHref( content, "div", { { "id", "fullannotation" } } ) << std::endl;
     //std::cout << parseHref( content, "span", { { "class", "buying-pricenew-val-number" } } ) << std::endl;
-    std::cout << parseHref( content, "div", { { "id", "product-image" } } ) << std::endl;
+    //std::cout << parseHref( content, "div", { { "id", "product-image" } } ) << std::endl;
+    std::cout << scbs.parseHref( content, "div", { { "id", "product" } } ) << std::endl;
 //    std::cout << parseHref( content, "", { { "", "" } } ) << std::endl;
+
     return 0;
 }
 
